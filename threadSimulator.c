10c0d79fa9f95e86c5c5f3aba9e411b40220dfe8
Esteban Mena <estebanmrj99@gmail.com>
2021-09-18 16:06:55 -0600

    Flujo basico

--- threadSimulator.c
+++ threadSimulator.c
@@ -5,8 +5,7 @@
 
 int totalTickets;
 int T;
-int done=0;
-
+int done = 0;
 
 typedef struct Function{
     coroutine* c;
@@ -21,12 +20,6 @@ typedef struct Stack{
     int stack[N];
 } Stack;
 
-typedef struct {
-    coroutine* c;
-    int max_x, max_y;
-    int x, y;
-} iter;
-
 Function *functions;
 Stack *stacks;
 
@@ -37,7 +30,6 @@ void readConsole(){
     scanf("%d",&T);
     functions =  (Function*) malloc(T * sizeof(Function)); 
     stacks = (Stack*) malloc(T * sizeof(Stack)); 
-
 
     for(int i = 0; i < T; i++)
     {
@@ -60,24 +52,56 @@ void readConsole(){
         printf("L: %d M: %ld F: %d X: %d\n",functions[i].L,functions[i].M,functions[i].F,functions[i].X);
 }
 
-void iterate(void* p) {
-  iter* it = (iter*)p;
-  int x,y;
-  for(x=0; x<it->max_x; x++) {
-    for(y=0; y < it->max_y; y++) {
-      it->x = x;
-      it->y = y;
-      yield(it->c);
+void pi(void* p){
+  
+    Function* f = (Function*)p;
+    long double n;
+    for(n = 1; n <= f -> M; n++){
+        if (n == f->M){
+            done++;
+            totalTickets -= f -> L;
+            f -> L = 0;
+        }
+        f->sum += 1/(n*n);
+        yield(f->c);
     }
-  }
 }
 
-void _sin(void* p){
+void napierian(void* p){
+    Function* f = (Function*)p;
+    long double n;
+    int signo = 1;
+    long upper = 1;
+    for(n = 1; n <= f -> M; n++){
+        if (n == f->M){
+            done++;
+            totalTickets -= f -> L;
+            f -> L = 0;
+        }
+        upper *= f->X;
+        f->sum += (upper / n) * signo;
+
+        signo *= -1;
+        yield(f->c);
+        
+    }
+}
+
+void euler(void* p){ // Que vergas hacer con el factorial a partir de 50
+    done++;
+}
+
+void _sin(void* p){ // Que vergas hacer con el factorial a partir de 50
     Function* f = (Function*)p;
     long double n,m;
     long double d,div;
     int signo = -1;
     for(n=0; n <= f->M ; n++){
+        if (n == f->M){
+            done++;
+            totalTickets -= f -> L;
+            f -> L = 0;
+        }
         d = 1;
         for(m = 0; m < 2*n+1 ; m++){
             d = d * f->X;
@@ -89,41 +113,10 @@ void _sin(void* p){
         signo *= -1;
         f->sum+= (d / div) * signo;
         yield(f->c);     
-          
     }
-    done++;
 }
 
-void napierian(void* p){
-    Function* f = (Function*)p;
-    long double n;
-    int signo = 1;
-    long upper = 1;
-    for(n = 1; n <= f -> M; n++){
-       
-        upper*= f->X;
-        f->sum+= (upper / n) * signo;
-
-        signo *= 1;
-        yield(f->c);
-        
-    }
-    //agregar al struct un done
-    done++;
-}
-
-void pi(void* p){
-  
-    Function* f = (Function*)p;
-    long double n;
-    for(n = 1; n <= f -> M; n++){
-        f->sum += 1/(n*n);
-        yield(f->c);
-    }
-    done++;
-}
-
-int chooseRandom(){ // Preguntarle a eddy si se puede repetir un ticket
+int chooseFunction(){ // Preguntarle a eddy si se puede repetir un ticket
     int ticketSelected = rand() % ((totalTickets - 1) + 1) + 1;
     
     // int flag = 1;
@@ -141,26 +134,49 @@ int chooseRandom(){ // Preguntarle a eddy si se puede repetir un ticket
             return i;
         }
     }
+
     return 0;
+}
+
+void gui(){
+    for(int i = 0; i < T; i++){
+        printf("%Lf \n", functions[i].sum);
+    }
+    printf("------------------------------------\n");
+    printf("------------------------------------\n");
+}
+
+void startLottery(){
+
+    for(int i = 0; i < T; i++){
+        int func = functions[i].F;
+
+        if(func == 1){
+            start(functions[i].c, &pi, &functions[i], stacks[i].stack+N);
+        } else if(func == 2){
+            start(functions[i].c, &napierian, &functions[i], stacks[i].stack+N);
+        } else if (func == 3){
+            start(functions[i].c, &euler, &functions[i], stacks[i].stack+N);
+        } else if(func == 4){
+            start(functions[i].c, &_sin, &functions[i], stacks[i].stack+N);
+        }
+    }
+
+    int choosedFunc = 0;
+
+    while (done < T)
+    {
+        choosedFunc = chooseFunction();
+        next(functions[choosedFunc].c);
+        gui();
+    }
 }
 
 int main(){
     srand(time(NULL));   // Seed para el random
     readConsole();
 
-
-    start(functions[0].c, &pi, &functions[0], stacks[0].stack+N);
-    while(next(functions[0].c)) {
-        printf("%LE \n", functions[0].sum);
-    }
-    printf("------------------------------------\n");
-    printf("------------------------------------\n");
-    start(functions[1].c, &napierian, &functions[1], stacks[1].stack+N);
-    while(next(functions[1].c)) {
-        printf("%LE \n", functions[1].sum);
- 
-    }
-
+    startLottery();
 
     free(functions);
     free(stacks);
