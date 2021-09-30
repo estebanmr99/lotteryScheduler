471dcac536b287bb8c3831126363228890be04f7
jossguzman <74888235+jossguzman@users.noreply.github.com>
2021-09-29 19:09:19 -0600

    Update threadSimulator.c

--- threadSimulator.c
+++ threadSimulator.c
@@ -3,24 +3,24 @@
 
 //---------------------------------------------------------------------------------------------------------------------------------------
 //Funcion que crea la ventana de interfaz para las series de taylor
+//---------------------------------------------------------------------------------------------------------------------------------------
+
 GtkWidget *createWindow(int width,int height){
     GtkWidget *window;
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL); // Define la ventana
-    gtk_window_set_title(GTK_WINDOW(window),"TAYLOR SERIES"); // Nombre de la ventana
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window),"TAYLOR SERIES");
     g_signal_connect(window,"destroy",gtk_main_quit,window);
-    gtk_window_set_default_size(GTK_WINDOW(window),width,height); // Define las dimensiones de la ventana
+    gtk_window_set_default_size(GTK_WINDOW(window),width,height);
     gtk_container_set_border_width(GTK_CONTAINER(window),50);
     return window;
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion que le aplica el css a la ventana de GTK
 void load_css(){
     GtkCssProvider *provider;
     GdkDisplay *display;
     GdkScreen *screen;
 
-    const gchar *file = "style.css"; // Carga archivo con estilos
+    const gchar *file = "style.css"; 
     GFile *css_fp = g_file_new_for_path(file);
     GError *error = 0;
 
@@ -29,36 +29,58 @@ void load_css(){
     screen = gdk_display_get_default_screen(display);
 
     gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider),GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    gtk_css_provider_load_from_file(provider,css_fp,&error); // Aplica los estilos
+    gtk_css_provider_load_from_file(provider,css_fp,&error);
 
     g_object_unref(provider);
+
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion que actualiza la barra de progreso y la suma en la interfaz
 void updateSum(Function *function){
     GtkOverlay *overlay = GTK_OVERLAY(function->bar->overlay);
     GtkWidget *progressBar = gtk_bin_get_child(GTK_BIN(overlay));
     GtkLabel *label = NULL;
     GList *children = gtk_container_get_children(GTK_CONTAINER(overlay));
+    gdouble fraction = 0.0;
 
     while(children){
         if(GTK_IS_LABEL(children->data)){
-            label = children->data; //Obtiene el widget donde poner la nueva data
+            label = children->data;
             break;
         }
         children = children->next;
     }
 
-    gdouble fraction = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(progressBar));
-    gchar *buffer = g_strdup_printf("Progress: %.0f%%  Sum: %0.9Le",fraction*100,function->sum); // El porcentaje por el que va y la suma hasta el momento
-    gtk_label_set_label(label,buffer);
-    g_free(buffer);
+    if(GTK_IS_PROGRESS_BAR(progressBar)){
+        fraction = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(progressBar));
+   
+        gchar *buffer = g_strdup_printf("Progress: %.0f%%  Sum: %0.20Le",fraction*100,function->sum);
+        if(fraction< 1.0){
+            if(label!=NULL){
+                gtk_label_set_label(label,buffer);
+            }
+            else{
+                gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(progressBar),TRUE);
+                gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progressBar),buffer);
+            }
+            gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressBar),fraction);
+
+        }
+        else{
+            if(label!=NULL){
+                gchar *buffer1 = g_strdup_printf("DONE  Sum: %0.20Le",function->sum);
+                gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progressBar),"100%");
+                gtk_label_set_label(label,buffer1);
+                g_free(buffer1);
+            }
+            gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressBar),fraction);
+        }
+        g_free(buffer);
+    }
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion que 
+
 void set_fract_callback(Function *function){
+    //printf("hoooollaaa");
     GtkOverlay *overlay = GTK_OVERLAY(function->bar->overlay);
     GtkWidget *progressBar = gtk_bin_get_child(GTK_BIN(overlay));
     GtkLabel *label = NULL;
@@ -78,7 +100,7 @@ void set_fract_callback(Function *function){
         fraction = gtk_progress_bar_get_fraction(GTK_PROGRESS_BAR(progressBar));
         fraction += 0.01;
 
-        gchar *buffer = g_strdup_printf("Progress: %.0f%%  Sum: %0.9Le",fraction*100,function->sum);
+        gchar *buffer = g_strdup_printf("Progress: %.0f%%  Sum: %0.20Le",fraction*100,function->sum);
         if(fraction< 1.0){
             if(label!=NULL){
                 gtk_label_set_label(label,buffer);
@@ -92,7 +114,7 @@ void set_fract_callback(Function *function){
         }
         else{
             if(label!=NULL){
-                gchar *buffer1 = g_strdup_printf("DONE - Sum: %0.9Le",function->sum);
+                gchar *buffer1 = g_strdup_printf("DONE  Sum: %0.20Le",function->sum);
                 gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progressBar),"100%");
                 gtk_label_set_label(label,buffer1);
                 g_free(buffer1);
@@ -103,21 +125,25 @@ void set_fract_callback(Function *function){
     }   
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion que maneja los widgets de progeso para cada serie de taylor
+
 gboolean handle_progress(GtkProgressBar *progressBar){
+
     gtk_progress_bar_pulse(progressBar);
-    gtk_progress_bar_set_pulse_step(progressBar,0.5);
+    gtk_progress_bar_set_pulse_step(progressBar,0.1);
+   // gtk_progress_bar_set_show_text(progressBar,TRUE);
     return TRUE;
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion que lee los parametros de la consola
+gboolean test(){
+    printf("hola\n");
+    return TRUE;
+}
+
 void readConsole(){
     int L,F,X;
     long int M;
     totalTickets = 0;
-    scanf("%d",&T); // Lee la T (cantidad de funciones siguientes que se deben de leer)
+    scanf("%d",&T);
     functions =  (Function*) malloc(T * sizeof(Function));
     stacks = (Stack*) malloc(T * sizeof(Stack));
     //int row = 0;
@@ -126,7 +152,7 @@ void readConsole(){
         X = 0;
         scanf("%d %ld %d", &L, &M, &F);
 
-        if(F != 1) // Si la funcion no es Pi se debe de leer el X
+        if(F != 1)
             scanf("%d", &X);
 
         totalTickets += L;
@@ -140,7 +166,7 @@ void readConsole(){
         functions[i].bar = (ProgressBars*) malloc(sizeof(ProgressBars));
         functions[i].bar->bar = gtk_progress_bar_new();
         functions[i].bar->overlay = gtk_overlay_new();
-        functions[i].bar->label = gtk_label_new("progressing...0%");
+        functions[i].bar->label = gtk_label_new("Waiting...0%");
         functions[i].bar->widget = gtk_button_new_with_mnemonic("Click");
         functions[i].bar->progress = gtk_button_new_with_mnemonic("Click");
         g_object_set(functions[i].bar->bar,"margin-top",15,NULL);
@@ -150,17 +176,19 @@ void readConsole(){
         gtk_overlay_add_overlay(GTK_OVERLAY(functions[i].bar->overlay),functions[i].bar->label);
         g_signal_connect_swapped(functions[i].bar->widget,"clicked",G_CALLBACK(set_fract_callback),(gpointer)&functions[i]);
         g_signal_connect_swapped(functions[i].bar->progress,"clicked",G_CALLBACK(updateSum),(gpointer)&functions[i]);
+
     }        
+    
+    //for(int i = 0; i < T; i++)
+    //    printf("L: %d M: %ld F: %d X: %d\n",functions[i].L,functions[i].M,functions[i].F,functions[i].X);
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion para representar la serie de taylor para Pi
 void pi(void* p){
-    Function* f = (Function*)p; // Guarda el puntero de la corutina
+    Function* f = (Function*)p;
     long int i = 1;
     long double n;
     for(n = 1; n <= f -> M; n++,i++){
-        if (n == f->M){ // Cuando sea la ultima iteracion remueva los tickets del total y incremente el contador de series completadas
+        if (n == f->M){
             done++;
             totalTickets -= f -> L;
             f -> L = 0;
@@ -168,25 +196,23 @@ void pi(void* p){
         f->sum += 1/(n*n);
 
         if(i==f->n){
-            gtk_button_clicked(GTK_BUTTON(f->bar->widget)); // boton invisible para indicar que termino el calculo de la serie
+            gtk_button_clicked(GTK_BUTTON(f->bar->widget));
             i=0;
         }
-        else    gtk_button_clicked(GTK_BUTTON(f->bar->progress)); // boton invisible para refrescar la interfaz
+        else    gtk_button_clicked(GTK_BUTTON(f->bar->progress));
         
-        yield(f->c); // "Retorna" la corutina (vease uso de comillas)
+        yield(f->c);
     }
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion para representar la serie de taylor para logaritmo natural
 void napierian(void* p){
-    Function* f = (Function*)p; // Guarda el puntero de la corutina
+    Function* f = (Function*)p;
     long int i = 1;
     long double n;
     int signo = 1;
     long upper = 1;
     for(n = 1; n <= f -> M; n++,i++){
-        if (n == f->M){ // Cuando sea la ultima iteracion remueva los tickets del total y incremente el contador de series completadas
+        if (n == f->M){
             done++;
             totalTickets -= f -> L;
             f -> L = 0;
@@ -196,25 +222,29 @@ void napierian(void* p){
 
         signo *= -1;
         if(i==f->n){
-            gtk_button_clicked(GTK_BUTTON(f->bar->widget)); // boton invisible para indicar que termino el calculo de la serie
+            gtk_button_clicked(GTK_BUTTON(f->bar->widget));
             i=0;
+
         }
 
-        else    gtk_button_clicked(GTK_BUTTON(f->bar->progress));// boton invisible para refrescar la interfaz
-        yield(f->c); // "Retorna" la corutina (vease uso de comillas)
+        else{
+            gtk_button_clicked(GTK_BUTTON(f->bar->progress));
+        }    
+        //gtk_button_clicked(GTK_BUTTON(f->bar->button));
+        
+        yield(f->c);
 
     }
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion para representar la serie de taylor para euler elevado a la X
-void euler(void* p){
-    Function* f = (Function*)p; // Guarda el puntero de la corutina
+void euler(void* p){ // EL factorial no debe de calcularse a mas de 1754
+    Function* f = (Function*)p;
     long int i = 1;
     long double m,n;
     long double d,div;
     for (n = 0; n <= f -> M; n++,i++) {
-        if (n == f->M) { // Cuando sea la ultima iteracion remueva los tickets del total y incremente el contador de series completadas
+        //printf("EULER\n");
+        if (n == f->M) {
             done++;
             totalTickets -= f -> L;
             f -> L = 0;
@@ -225,32 +255,35 @@ void euler(void* p){
         }
         div = 1;
         long double newN = n;
-        if (newN > (long double)1753) { // El calculo maximo para un factorial que cabe en un long double es de 1754
+        if (newN > (long double)1753) {
             newN = (long double)1754;
         }
-        for (m = 1; m <= newN; m++) {
+        for (m = 1; m < newN; m++) {
             div = div * m;
         }
-        f->sum += (d / div);
-        if(i == f->n){
-            gtk_button_clicked(GTK_BUTTON(f->bar->widget)); // boton invisible para indicar que termino el calculo de la serie
-            i = 0;
+        f->sum+= (d / div);
+        if(i==f->n){
+            gtk_button_clicked(GTK_BUTTON(f->bar->widget));
+            i=0;
+            //printf("AUMENTA %% EULER");
         }
-        else    gtk_button_clicked(GTK_BUTTON(f->bar->progress)); // boton invisible para refrescar la interfaz
-        yield(f->c); // "Retorna" la corutina (vease uso de comillas)
+
+        else{
+            gtk_button_clicked(GTK_BUTTON(f->bar->progress));
+            //printf("NO AUMENTA %% EULER");
+        }    
+        yield(f->c);
     }
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-//Funcion para representar la serie de taylor de seno
 void _sin(void* p){
-    Function* f = (Function*)p; // Guarda el puntero de la corutina
+    Function* f = (Function*)p;
     long int i = 1;
     long double m,n;
     long double d,div;
     int signo = -1;
-    for(n=0; n <= f->M ; n++){
-        if (n == f->M){ // Cuando sea la ultima iteracion remueva los tickets del total y incremente el contador de series completadas
+    for(n=0; n <= f->M ; n++,i++){
+        if (n == f->M){
             done++;
             totalTickets -= f -> L;
             f -> L = 0;
@@ -260,8 +293,8 @@ void _sin(void* p){
             d = d * f->X;
         }
         div = 1;
-        long double newN = 2*n+1;
-        if (newN > (long double)1753){ // El calculo maximo para un factorial que cabe en un long double es de 1754
+        long double newN = 2*n+1; // EL factorial no debe de calcularse a mas de 1754
+        if (newN > (long double)1753){
             newN = (long double)1754;
         }
         for(m = 1; m <= newN; m++){
@@ -270,16 +303,32 @@ void _sin(void* p){
         signo *= -1;
         f->sum+= (d / div) * signo;
         if(i==f->n){
-            gtk_button_clicked(GTK_BUTTON(f->bar->widget)); // boton invisible para indicar que termino el calculo de la serie
+            //printf("sen aumenta %%\n");
+            gtk_button_clicked(GTK_BUTTON(f->bar->widget));
             i=0;
+            
         }
-        else gtk_button_clicked(GTK_BUTTON(f->bar->progress)); // boton invisible para refrescar la interfaz
+        else{
+            //printf("no aumenta sen\n");
+            gtk_button_clicked(GTK_BUTTON(f->bar->progress));
+        }
         
-        yield(f->c); // "Retorna" la corutina (vease uso de comillas)
+        yield(f->c);
     }
 }
 
-//---------------------------------------------------------------------------------------------------------------------------------------
+int chooseFunction(){
+    int ticketSelected = rand() % ((totalTickets - 1) + 1) + 1;
+    int sum = 0;
+    for (int i = 0; i < T; i++){
+        sum += functions[i].L;
+        if(sum >= ticketSelected){
+            return i;
+        }
+    }
+    return 0;
+}
+
 //Funcion para escoger la funcion que gano la loteria
 int chooseFunction(){
     int ticketSelected = rand() % ((totalTickets - 1) + 1) + 1; //Random de 1 a l total de tiquetes
